b0dd35a5a2fb405221dd36cde565dc4bc6027029
Archimedes Trajano <developer@trajano.net>
2021-06-29 17:13:03 -0400

    added sqlite-async and adapted typings

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import openDatabase from "websql";
-const db = openDatabase("mydb.db", "1.0", "description", 1);
+import { openDatabaseAsync } from "./sqlite-async";
 
 const table = `foo${Math.floor(Math.random() * 100000)}`;
 const sql1 = `create table if not exists ${table} ( id int primary key )`;
@@ -8,8 +8,9 @@ const sql2 = `insert into ${table} (id) values (${Math.floor(
 )})`;
 const sql3 = `select * from ${table}`;
 
-const p = async () =>
+const callbackHell = async () =>
   new Promise<void>((resolve, reject) => {
+    const db = openDatabase("mydb.db", "1.0", "description", 1);
     db.transaction(
       (tx: any) => {
         tx.executeSql(
@@ -52,11 +53,64 @@ const p = async () =>
         reject(err);
         return true;
       },
-      (txObj: any) => {
+      () => {
         console.error("Success");
         resolve();
       }
     );
   });
 
-setTimeout(p, 0);
+const callbackHell2 = async () =>
+  new Promise<void>((resolve, reject) => {
+    openDatabaseAsync("mydb.db", "1.0").then((db) => {
+      db.transaction(
+        (tx: any) => {
+          tx.executeSql(
+            sql1,
+            [],
+            (tx1: any, rs: any) => {
+              tx1.executeSql(
+                sql2,
+                [],
+                (tx2: any, rs: any) => {
+                  tx2.executeSql(
+                    sql3,
+                    [],
+                    (tx3: any, rs: any) => {
+                      console.log(rs.rows.item(0));
+                    },
+                    (err: any) => {
+                      console.error("ERROR " + sql3, err);
+                      reject(err);
+                      return true;
+                    }
+                  );
+                },
+                (err: any) => {
+                  console.error("ERROR " + sql2, err);
+                  reject(err);
+                  return true;
+                }
+              );
+            },
+            (err: any) => {
+              console.error("ERROR " + sql1, err);
+              reject(err);
+              return true;
+            }
+          );
+        },
+        (err: any) => {
+          console.error("ERROR", err);
+          reject(err);
+          return true;
+        },
+        () => {
+          console.error("Success");
+          resolve();
+        }
+      );
+    });
+  });
+
+setTimeout(callbackHell2, 0);
--- src/websql.d.ts
+++ src/websql.d.ts
@@ -1,2 +1,48 @@
 // @types/websql does not appear to work correctly
-declare module "websql";
+declare module "websql" {
+  declare class WebSQLTransaction {
+    executeSql(
+      sqlStatement: string,
+      args?: any[],
+      callback?: SQLStatementCallback,
+      errorCallback?: SQLStatementErrorCallback
+    );
+  }
+  declare class SQLStatementCallback {}
+  declare class SQLStatementErrorCallback {}
+  declare class SQLResultSetRowList {
+    item(i: index): Object;
+  }
+  declare class SQLResultSet {
+    rows: SQLResultSetRowList;
+  }
+  export type SQLTransactionCallback = (tx: WebSQLTransaction) => void;
+  export type SQLTransactionErrorCallback = (err: any) => void;
+  export type SQLVoidCallback = () => void;
+
+  export type SQLTransactionCallback = (
+    tx: WebSQLTransaction,
+    rs: SQLResultSet
+  ) => void;
+
+  declare class WebSQLDatabase {
+    transaction(
+      callback: SQLite.SQLTransactionCallback,
+      errorCallback?: SQLite.SQLTransactionErrorCallback,
+      successCallback?: SQLite.SQLVoidCallback
+    );
+    readTransaction(
+      callback: SQLite.SQLTransactionCallback,
+      errorCallback?: SQLite.SQLTransactionErrorCallback,
+      successCallback?: SQLite.SQLVoidCallback
+    );
+  }
+
+  export default function openDatabase(
+    name: string,
+    version?: string,
+    description?: string,
+    size?: number,
+    callback?: (db: WebSQLDatabase) => void
+  ): WebSQLDatabase;
+}
